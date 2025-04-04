478603e9dc81fc017717bc41be628877bf7797d2
francois-prs <piresf@me.com>
2025-04-04 11:49:33 +0200

    creation of MVC folders/packages and Timezone configuration

--- src/main/java/edu/fprs/project_loanit_back/ProjectLoaniTBackApplication.java
+++ src/main/java/edu/fprs/project_loanit_back/ProjectLoaniTBackApplication.java
@@ -1,13 +1,21 @@
 package edu.fprs.project_loanit_back;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ProjectLoaniTBackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectLoaniTBackApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
 }

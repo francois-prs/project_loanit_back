26197449b1a746b3cddfcbd90136bfb2d3ca5dbe
francois-prs <piresf@me.com>
2025-04-04 09:27:29 +0200

    Initial commit with Spring Boot Application structure

---
+++ src/main/java/edu/fprs/project_loanit_back/ProjectLoaniTBackApplication.java
@@ -0,0 +1,13 @@
+package edu.fprs.project_loanit_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLoaniTBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLoaniTBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/fprs/project_loanit_back/ProjectLoaniTBackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fprs.project_loanit_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLoaniTBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

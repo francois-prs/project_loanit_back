ec33b0dfbfe3ec9af06aec9102405102b3dbf90a
francois-prs <piresf@me.com>
2025-04-15 00:27:40 +0200

    update Equipment.java with ManytoOne ConditionEquipment

--- src/main/java/edu/fprs/project_loanit_back/model/ConditionEquipment.java
+++ src/main/java/edu/fprs/project_loanit_back/model/ConditionEquipment.java
@@ -2,12 +2,14 @@ package edu.fprs.project_loanit_back.model;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import edu.fprs.project_loanit_back.view.Affichage;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class ConditionEquipment {
 
     @Id
